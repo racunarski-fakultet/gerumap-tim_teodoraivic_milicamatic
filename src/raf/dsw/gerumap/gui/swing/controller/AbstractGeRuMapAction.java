f19e6c39c2facafc6abeead5d279e1fc1e221084
Teodora Ivic <teodoraivic0310@gmail.com>
2022-10-23 16:27:00 +0200

    Implementirana AbstractGeRuMapAction klasa

--- src/raf/dsw/gerumap/gui/swing/controller/AbstractGeRuMapAction.java
+++ src/raf/dsw/gerumap/gui/swing/controller/AbstractGeRuMapAction.java
@@ -1,6 +1,23 @@
 package raf.dsw.gerumap.gui.swing.controller;
-import javax.swing.AbstractAction;
+import javax.swing.*;
+import java.net.URL;
 
 
 public abstract class AbstractGeRuMapAction extends AbstractAction {//osnovna implementacija action listenera
+
+    public Icon loadIcon(String FileName){
+
+        URL imageURL = getClass().getResource(FileName);
+        Icon icon = null;
+
+        if(imageURL != null){
+            icon = new ImageIcon(imageURL);
+        }
+        else{
+            System.err.println("Resource not found: " + FileName);
+        }
+
+        return icon;
+
+    }
 }
