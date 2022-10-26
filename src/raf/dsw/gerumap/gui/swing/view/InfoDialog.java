91b4086081daa40f7b4689e0d8572500f9c96d0b
Teodora Ivic <teodoraivic0310@gmail.com>
2022-10-26 19:48:01 +0200

    InfoDialog

---
+++ src/raf/dsw/gerumap/gui/swing/view/InfoDialog.java
@@ -0,0 +1,13 @@
+package raf.dsw.gerumap.gui.swing.view;
+
+import javax.swing.*;
+
+public class InfoDialog extends JDialog {
+
+    public InfoDialog(JFrame parent){
+        super(parent, "Info", false);
+
+        setSize(400,300);
+    }
+
+}
