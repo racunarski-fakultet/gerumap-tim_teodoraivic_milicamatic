5491ac00d3841c298398e0b4cb8fc1b642e95c17
Milica Matic <milica.matic0202@gmail.com>
2022-12-12 22:06:13 +0100

    zapoceta Implementacija paintera

--- src/main/java/raf/dsw/gerumap/gui/swing/view/painters/ConceptPainter.java
+++ src/main/java/raf/dsw/gerumap/gui/swing/view/painters/ConceptPainter.java
@@ -1,5 +1,6 @@
 package raf.dsw.gerumap.gui.swing.view.painters;
 
+import raf.dsw.gerumap.gui.swing.view.MapView;
 import raf.dsw.gerumap.gui.swing.view.painters.Painter;
 import raf.dsw.gerumap.repository.implementation.Concept;
 import raf.dsw.gerumap.repository.implementation.Element;
@@ -14,31 +15,42 @@ public class ConceptPainter extends Painter {
     private Shape shape;
 
 
-    public ConceptPainter(Element element, Shape s, Concept concept) {
-        super(element, s);
+    public ConceptPainter(Element element,MapView mapView) {
+        super(element,mapView);
 
-        this.shape=new Ellipse2D.Float(concept.getX(),concept.getY(),concept.getWidth(),concept.getHeight());
+        concept=(Concept)element;
+
+        this.shape=new Ellipse2D.Float(concept.getX(),concept.getY(), concept.getWidth(), concept.getHeight());
     }
 
     @Override
-    public void elementAt(float x, float y) {
-        super.elementAt(x, y);
+    public boolean elementAt( float x, float y) {
+        return super.elementAt( x, y);
     }
 
     @Override
     public void draw(Graphics2D g) {
 
-
-        Graphics2D g2 = (Graphics2D) g;
-        Font f=new Font(concept.getName(),Font.PLAIN,10);
+        g.setPaint(Color.black);
         BasicStroke stroke=new BasicStroke(concept.getStroke());
-        g2.setStroke(stroke);
-        g2.setColor(Color.CYAN);
-        g2.setFont(f);
+        g.setStroke(stroke);
+        g.draw(shape);
 
-        g2.draw(shape);
-        //g2.drawString(concept.getName(),concept.getX()+5,concept.getY()+5);
+        Font f=new Font(concept.getName(),Font.PLAIN,10);
+
+        g.setFont(f);
+
+        g.drawString(concept.getName(),concept.getX()+20,concept.getY()+20);
+
+    }
 
 
+
+    public Shape getShape() {
+        return shape;
+    }
+
+    public void setShape(Shape shape) {
+        this.shape = shape;
     }
 }
